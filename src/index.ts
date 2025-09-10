8f32a3b6c658b3496d710009bf6fa2bbe21b4556
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2025-09-10 14:43:55 -0700

    Using messages instead of prompt

--- src/index.ts
+++ src/index.ts
@@ -1,110 +1,116 @@
 import { DeepgramSTT, TextComponent, RealtimeKitTransport, ElevenLabsTTS, RealtimeAgent } from '@cloudflare/realtime-agents';
+import { runWithTools } from "@cloudflare/ai-utils";
 
 class MyTextProcessor extends TextComponent {
-  env: Env;
+	env: Env;
 
-  constructor(env: Env) {
-    super();
-    this.env = env;
-  }
+	constructor(env: Env) {
+		super();
+		this.env = env;
+	}
 
-  async onTranscript(text: string, reply: (text: string) => void) {
+	async onTranscript(text: string, reply: (text: string) => void) {
+		console.log("Transcript from User: ", text);
+		const messages = [
+			{ role: 'system', content: 'You are a helpful assistant respond concisely and do not repeat yourself. ' },
+			{ role: 'user', content: text },
+		];
 
+		const { response } = await this.env.AI.run('@cf/meta/llama-3.1-8b-instruct', {
+			messages,
+		});
 
+		console.log("Response: ", response);
 
-    const { response } = await this.env.AI.run('@cf/meta/llama-3.1-8b-instruct', {
-
-      prompt: text,
-    });
-    reply(response!);
-  }
+		reply(response!);
+	}
 }
 
 export class MyAgent extends RealtimeAgent<Env> {
-  constructor(ctx: DurableObjectState, env: Env) {
-    super(ctx, env);
-  }
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env);
+	}
 
-  async init(agentId: string, meetingId: string, authToken: string, workerUrl: string, accountId: string, apiToken: string) {
-    // Construct your text processor for generating responses to text
-    const textProcessor = new MyTextProcessor(this.env);
-    // Construct a Meeting object to join the RTK meeting
-    const rtkTransport = new RealtimeKitTransport(meetingId, authToken);
+	async init(agentId: string, meetingId: string, authToken: string, workerUrl: string, accountId: string, apiToken: string) {
+		// Construct your text processor for generating responses to text
+		const textProcessor = new MyTextProcessor(this.env);
+		// Construct a Meeting object to join the RTK meeting
+		const rtkTransport = new RealtimeKitTransport(meetingId, authToken);
 
-    // Construct a pipeline to take in meeting audio, transcribe it using
-    // Deepgram, and pass our generated responses through ElevenLabs to
-    // be spoken in the meeting
-    await this.initPipeline(
-      [
-        rtkTransport,
-        new DeepgramSTT(this.env.DEEPGRAM_API_KEY),
-        textProcessor,
-        new ElevenLabsTTS(this.env.ELEVENLABS_API_KEY),
-        rtkTransport,
-      ],
-      agentId,
-      workerUrl,
-      accountId,
-      apiToken,
-    );
+		// Construct a pipeline to take in meeting audio, transcribe it using
+		// Deepgram, and pass our generated responses through ElevenLabs to
+		// be spoken in the meeting
+		await this.initPipeline(
+			[
+				rtkTransport,
+				new DeepgramSTT(this.env.DEEPGRAM_API_KEY),
+				textProcessor,
+				new ElevenLabsTTS(this.env.ELEVENLABS_API_KEY),
+				rtkTransport,
+			],
+			agentId,
+			workerUrl,
+			accountId,
+			apiToken
+		);
 
-    const { meeting } = rtkTransport;
+		const { meeting } = rtkTransport;
 
-    // The RTK meeting object is accessible to us, so we can register handlers
-    // on various events like participant joins/leaves, chat, etc.
-    // This is optional
-    meeting.participants.joined.on('participantJoined', (participant) => {
-      textProcessor.speak(`Participant Joined ${participant.name}`);
-    });
-    meeting.participants.joined.on('participantLeft', (participant) => {
-      textProcessor.speak(`Participant Left ${participant.name}`);
-    });
+		// The RTK meeting object is accessible to us, so we can register handlers
+		// on various events like participant joins/leaves, chat, etc.
+		// This is optional
+		meeting.participants.joined.on('participantJoined', (participant) => {
+			textProcessor.speak(`Participant Joined ${participant.name}`);
+		});
+		meeting.participants.joined.on('participantLeft', (participant) => {
+			textProcessor.speak(`Participant Left ${participant.name}`);
+		});
 
-    // Make sure to actually join the meeting after registering all handlers
-    await meeting.join();
-  }
+		// Make sure to actually join the meeting after registering all handlers
+		await meeting.join();
+	}
 
-  async deinit() {
-    // Add any other cleanup logic required
-    await this.deinitPipeline();
-  }
+	async deinit() {
+		// Add any other cleanup logic required
+		await this.deinitPipeline();
+	}
 }
 
 export default {
-  async fetch(request, env, _ctx): Promise<Response> {
-    const url = new URL(request.url);
-    const meetingId = url.searchParams.get('meetingId');
-    if (!meetingId) {
-      return new Response(null, { status: 400 });
-    }
+	async fetch(request, env, _ctx): Promise<Response> {
+		const url = new URL(request.url);
+		const meetingId = url.searchParams.get('meetingId');
+		if (!meetingId) {
+			return new Response(null, { status: 400 });
+		}
 
-    const agentId = meetingId;
-    const agent = env.MY_AGENT.idFromName(meetingId);
-    const stub = env.MY_AGENT.get(agent);
-    // The fetch method is implemented for handling internal pipeline logic
-    if (url.pathname.startsWith('/agentsInternal')) {
-      return stub.fetch(request);
-    }
+		const agentId = meetingId;
+		const agent = env.MY_AGENT.idFromName(meetingId);
+		const stub = env.MY_AGENT.get(agent);
+		// The fetch method is implemented for handling internal pipeline logic
+		if (url.pathname.startsWith('/agentsInternal')) {
+			return stub.fetch(request);
+		}
 
-    // Your logic continues here
-    switch (url.pathname) {
-      case '/init':
-        // This is the authToken for joining a meeting, it can be passed
-        // in query parameters as well if needed
-        const authHeader = request.headers.get('Authorization');
-        if (!authHeader) {
-          return new Response(null, { status: 401 });
-        }
+		// Your logic continues here
+		switch (url.pathname) {
+			case '/init':
+				// This is the authToken for joining a meeting, it can be passed
+				// in query parameters as well if needed
+				const authHeader = request.headers.get('Authorization');
+				if (!authHeader) {
+					return new Response(null, { status: 401 });
+				}
 
-        // We just need the part after `Bearer `
-        await stub.init(agentId, meetingId, authHeader.split(' ')[1], url.host, env.ACCOUNT_ID, env.API_TOKEN);
+				// We just need the part after `Bearer `
+				await stub.init(agentId, meetingId, authHeader.split(' ')[1], url.host, env.ACCOUNT_ID, env.API_TOKEN);
 
-        return new Response(null, { status: 200 });
-      case '/deinit':
-        await stub.deinit();
-        return new Response(null, { status: 200 });
-    }
+				return new Response(null, { status: 200 });
+			case '/deinit':
+				await stub.deinit();
+				return new Response(null, { status: 200 });
+		}
 
-    return new Response(null, { status: 404 });
-  },
+		return new Response(null, { status: 404 });
+	},
 } satisfies ExportedHandler<Env>;
